ca0f995e0a08b7599cbccc53dc3ac45b81d76c71
rahim-alam10 <rahimalam010@gmail.com>
2026-07-24 19:33:39 +0500

    Initial commit

---
+++ FirstServer/index.js
@@ -0,0 +1,16 @@
+// My first Server
+
+const express = require('express')
+ 
+const app =  express()
+const port = 3000
+
+// Define a basic route
+app.get('/', (req, res) => {
+  res.send('Express server is successfully running!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`);
+});
