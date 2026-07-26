7ce906bb9e944f80a48801e910be73414158a255
rahim-alam10 <rahimalam010@gmail.com>
2026-07-26 19:28:35 +0500

    Handling Asyn  Calls

---
+++ FirstServer/httpRequest.js
@@ -0,0 +1,25 @@
+import http from 'http';
+
+const options = {
+  hostname: 'jsonplaceholder.typicode.com',
+  path: '/todos/1',
+  method: 'GET'
+};
+
+const req = http.request(options, (res) => {
+  let data = '';
+
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  res.on('end', () => {
+    console.log('Response:', data);
+  });
+});
+
+req.on('error', (error) => {
+  console.error('Error:', error.message);
+});
+
+req.end();
