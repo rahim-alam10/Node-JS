68cec4900d9215fb0b6fc4065e325fa00c064895
rahim-alam10 <rahimalam010@gmail.com>
2026-07-31 02:17:38 +0500

    File Handling

--- FirstServer/index.js
+++ FirstServer/index.js
@@ -1,10 +1,18 @@
 // My first Server
-
 const express = require('express')
- 
+const http = require("http")
+const fs = require("fs")
+
 const app =  express()
 const port = 3000
 
+const myServer = http.createServer((req, res) => {
+  const log = `${Date.now()}: New Reuest Recived`;
+  fs.appendFile("log.txt", log, (err, res) => {
+    res.end("Hello from server")
+  });
+
+})
 // Define a basic route
 app.get('/', (req, res) => {
   res.send('Express server is successfully running!');
