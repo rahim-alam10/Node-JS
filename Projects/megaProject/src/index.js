ce4a7cd6d044388ce026a7c803bbb5325507aac4
rahim-alam10 <rahimalam010@gmail.com>
2026-08-01 01:24:13 +0500

    Projects on backend

---
+++ Projects/megaProject/src/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERR:", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed:", error);
+  }); 
