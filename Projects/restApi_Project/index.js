6415e4eebee876518254e97333b214d2c47ac39f
rahim-alam10 <rahimalam010@gmail.com>
2026-08-02 15:20:12 +0500

    Server Side Rendering

--- Projects/restApi_Project/index.js
+++ Projects/restApi_Project/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import userRouter from "./routes/userRouter.js";
 import connectDb from "./db/connectDb.js";
 import logReqRes from "./middleWares/logReqRes.js";
+import path from "path";
+import User from "./models/user.js";
 
 const app = express();
 const PORT = 8000;
@@ -9,9 +11,17 @@ const PORT = 8000;
 //Connection
 connectDb("mongodb://127.0.0.1:27017/backend").then(()=> console.log("MongoDb Connected Successfully...."))
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"))
+
+app.use(express.json())
+
+app.get("/test", async (req, res) => {
+    const allUsers = await User.find({});
+    return res.render('home', { users: allUsers })
+})
 
 // Middleware - Plugin    
-app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(logReqRes("log.txt"));
     
