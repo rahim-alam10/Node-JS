7fe3e6dd209685f4d4091e9586d55f103fc836a1
rahim-alam10 <rahimalam010@gmail.com>
2026-08-08 19:51:46 +0500

    Add new routes to user  controller

--- Projects/megaProject/src/routes/user.routes.js
+++ Projects/megaProject/src/routes/user.routes.js
@@ -1,11 +1,22 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+import { changeCurrentPassword, 
+         getCurrentUser, 
+         getUserChannelProfile, 
+         getWatchHistory, 
+         loginUser, 
+         logoutUser, 
+         refreshAccessToken, 
+         registerUser, 
+         updateAccountDetails, 
+         updateUserAvatar, 
+         updateUserCoverImage } 
+from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const router = Router();
 
-router.post("/register", 
+router.post("/register",
     upload.fields([         // middle ware to check for the avatar and coverImage present or not
         {
             name: "avatar",
@@ -22,9 +33,15 @@ router.post("/login", loginUser)
 
 
 // Secure routes
-router.post("/logout", verifyJWT , logoutUser)
+router.post("/logout", verifyJWT, logoutUser)
 router.post("/refresh-token", refreshAccessToken)
-
+router.post("/change-passwrod", verifyJWT, changeCurrentPassword)
+router.get("/current-user", verifyJWT, getCurrentUser)
+router.patch("/update-account", verifyJWT, updateAccountDetails)
+router.patch("/avatar", verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.patch("/cover-image", upload.single("cover-image"), verifyJWT, updateUserCoverImage)
+router.get("/c/:username", verifyJWT, getUserChannelProfile)
+router.get("/history", verifyJWT, getWatchHistory)
 
 export {
     router
