d678234510cd919a9bc17d7670567dd8ee868cae
rahim-alam10 <rahimalam010@gmail.com>
2026-08-10 00:39:22 +0500

    fixed ownwe spelling

--- Projects/megaProject/src/models/comment.model.js
+++ Projects/megaProject/src/models/comment.model.js
@@ -13,7 +13,7 @@ const commentSchema = new Schema(
             ref: "Video",
         },
 
-        ownwe: {
+        ownwer: {
             type: Schema.Types.ObjectId,
             ref: "User"
         }
